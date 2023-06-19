d6bbbfffc56e0ec8ada84fa935b2ae9020c39697
nd-92 <nathan.duggins1992@hotmail.co.uk>
2023-06-19 23:53:37 +0100

    u

--- IOControl.C
+++ IOControl.C
@@ -21,10 +21,13 @@ void writeScalarArray(
     const scalarArray &f,
     const arrayLabel meshDim[2])
 {
+    // Open .dat file and set write precision
     ofstream toWrite;
     const string writeName = fileName + ".dat";
     toWrite.open(writeName);
     toWrite << setprecision(100);
+
+    // Loop over all indices printing value of array
     for (unsigned i = 0; i < meshDim[0]; i++)
     {
         for (unsigned j = 0; j < meshDim[1]; j++)
@@ -33,5 +36,8 @@ void writeScalarArray(
         }
         toWrite << endl;
     }
+    toWrite << endl;
+
+    // Close file
     toWrite.close();
 }
